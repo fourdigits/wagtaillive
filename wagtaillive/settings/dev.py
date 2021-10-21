6f2bf1f4b3af6143e3bb9ab5996948df8526b68e
Coen van der Kamp <coen@fourdigits.nl>
2021-10-21 20:32:07 +0200

    Initial commit

---
+++ wagtaillive/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ym^$+otfouq8)air1v&6llaf^k2n&yup-a#pdgy8*0*91&n*t5'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
