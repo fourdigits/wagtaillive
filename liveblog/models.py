ca44f28c31f07fd2a4c92ce953c1aea5d4dce40a
Coen van der Kamp <coen@fourdigits.nl>
2021-10-21 20:34:39 +0200

    Add liveblog page

--- liveblog/models.py
+++ liveblog/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from wagtail.core.models import Page
 
-# Create your models here.
+from wagtail_live.models import LivePageMixin
+
+class LiveBlogPage(Page, LivePageMixin):
+    content_panels = Page.content_panels + LivePageMixin.panels
