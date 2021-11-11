a02f511efcb482a10b4a408928ebd362e569395b
Coen van der Kamp <coen@fourdigits.nl>
2021-11-11 15:08:28 +0100

    Add base template to loaded app

--- home/models.py
+++ home/models.py
@@ -2,7 +2,6 @@ from wagtail.core.models import Page
 from wagtail_live.utils import get_live_page_model
 
 class HomePage(Page):
-
     def get_context(self, request, *args, **kwargs):
         ctx = super().get_context(request, *args, **kwargs)
         ctx.update({
